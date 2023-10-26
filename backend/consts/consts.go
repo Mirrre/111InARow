ccdcd07ff3d6faffd71b12f32bdba80966b6aa36
lps <2396258912@qq.com>
2023-10-26 20:21:25 +0800

    初始化后端结构以及完成登录功能

---
+++ backend/consts/consts.go
@@ -0,0 +1,4 @@
+package consts
+
+const AccessKey = "UbG9NVTSj7i4kJ_BAUWcaQi6CrVPgOPRh5eWrDdA"
+const SecretKey = "UxuMXKJ4Sx7gAPmBht4mzEKWaK9D9_MNGXQpy8lo"
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"app/config"
+	"app/controllers"
+	"log"
+)
 
 func main() {
-	fmt.Println("helloworld")
+	// 初始化数据库
+	dsn := config.SetDsn()
+	db, err := config.InitDatabase(dsn)
+	//user := models.User{
+	//	Username: "lps",
+	//	Password: "666",
+	//}
+	//db.Create(&user)  创建成功
+	if err != nil {
+		log.Fatalf("无法连接数据库: %s\n", err)
+	}
+	// 初始化路由函数
+	r := config.InitGinEngine(db)
+	r.POST("/user/register/", controllers.Register)
+	r.POST("/user/login", controllers.Login)
+	err = r.Run(":8080")
+	if err != nil {
+		panic("启动服务失败")
+	}
 }
