7603bb6afe0728ff55359d8fe9500ae837668ef8
ql17774641928 <2722937531@qq.com>
2023-11-07 20:25:11 +0800

    long前端后端

--- backend_node/src/dao/GameVideoDao.ts
+++ backend_node/src/dao/GameVideoDao.ts
@@ -3,6 +3,7 @@ import { BaseDao, Dao, Sql } from '@snow';
 @Dao('game_video')
 export class GameVideoDao extends BaseDao {
     // @Sql(`select * from game_video`)
-    @Sql(`SELECT all_video.* FROM all_video LEFT JOIN game_video ON all_video.id = game_video.all_id;`)
+    // @Sql(`SELECT all_video.* FROM all_video LEFT JOIN game_video ON all_video.id = game_video.all_id;`)
+    @Sql(`SELECT * FROM game_video JOIN videos ON videos.id = game_video.all_id`)
     async getGameVideo(): Promise<any> {}
 }
--- backend_node/src/dao/PhysicalVideoDao.ts
+++ backend_node/src/dao/PhysicalVideoDao.ts
@@ -3,6 +3,7 @@ import { BaseDao, Dao, Sql } from '@snow';
 @Dao('physical_video')
 export class PhysicalVideoDao extends BaseDao {
     // @Sql(`select * from physical_video`)
-    @Sql(`SELECT all_video.* FROM all_video LEFT JOIN physical_video ON all_video.id = physical_video.all_id;`)
+    // @Sql(`SELECT all_video.* FROM all_video LEFT JOIN physical_video ON all_video.id = physical_video.all_id;`)
+    @Sql(`SELECT * FROM physical_video JOIN videos ON videos.id = physical_video.all_id`)
     async getPhysicalVideo(): Promise<any> {}
 }
